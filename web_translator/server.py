01191307e14a8e2a5dc65a1544df7a37a88c49fe
Ilton ingui <iltoningui@outlook.com>
2023-03-12 02:10:00 +0100

    Update server.py

--- web_translator/server.py
+++ web_translator/server.py
@@ -1,43 +1,41 @@
-# """
-#     Contains the translations actions
-# """
-# from flask import request
-# from .pkg.translation import en_to_fr, fr_to_en, get_translator
+from flask import request
+from flask import Flask
+from pkg.translation import en_to_fr, fr_to_en, get_translator
 
-# from flask import Flask
-# app = Flask(__name__)
+app = Flask('web translator')
 
 
-# @app.route("/englishToFrench")
-# def english_to_french():
-#     """
-#         Endpoint to translate english to french
+@app.route("/englishToFrench")
+def english_to_french():
+    """
+        Endpoint to translate english to french
 
-#     Returns:
-#         str: the text translated to french
-#     """
-#     text = request.args.get('text')
+    Returns:
+        str: the text translated to french
+    """
+    text = request.args.get('text')
 
-#     translator = get_translator()
+    translator = get_translator()
 
-#     en_fr_translated_text = en_to_fr(text, translator)
+    en_fr_translated_text = en_to_fr(text, translator)
 
-#     return en_fr_translated_text
+    return en_fr_translated_text
 
-# @app.route("/frenchToEnglish")
-# def french_to_english():
-#     """
-#         Endpoint to translate french to english
+@app.route("/frenchToEnglish")
+def french_to_english():
+    """
+        Endpoint to translate french to english
 
-#     Returns:
-#         str: the text translated to english
-#     """
-#     text = request.args.get('text')
+    Returns:
+        str: the text translated to english
+    """
+    text = request.args.get('text')
 
-#     translator = get_translator()
+    translator = get_translator()
 
-#     fr_en_translated_text = fr_to_en(text, translator)
+    fr_en_translated_text = fr_to_en(text, translator)
 
-#     return fr_en_translated_text
+    return fr_en_translated_text
 
-# app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
